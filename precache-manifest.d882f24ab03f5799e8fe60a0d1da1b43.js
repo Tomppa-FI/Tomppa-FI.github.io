d6517bf4995e279919ee1864bde2e71d92d279cf
Tomppa-FI <TomppaFI@protonmail.com>
2020-06-08 10:13:55 +0300

    Updates

---
+++ precache-manifest.d882f24ab03f5799e8fe60a0d1da1b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fe2951d417c1c88ea034860a9729e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea080abd3e3052397763",
+    "url": "/static/css/main.a3b1d93f.chunk.css"
+  },
+  {
+    "revision": "973c6a0e913f86c2ab2c",
+    "url": "/static/js/2.34c53e83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34c53e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea080abd3e3052397763",
+    "url": "/static/js/main.907d6e76.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
--- src/MediaQueries.ts
+++
@@ -1,6 +0,0 @@
-export const MediaQueryMap = {
-  small: "@media (min-width: 576px)",
-  medium: "@media (min-width: 768px)",
-  standard: "@media (min-width: 992px)",
-  large: "@media (min-width: 1200px)"
-}
